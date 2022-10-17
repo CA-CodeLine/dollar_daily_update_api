126d1c422752eeaedd47f41a97f8f189c3edab44
Andre Oliveira <andreoliveirar2d2@gmail.com>
2022-10-16 22:10:33 -0300

    feat: adding the gateway for dollar today

---
+++ app/gateways/dollar_today_gateway.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# DollarTodayGateway
+class DollarTodayGateway
+  def initialize
+    @agent = Mechanize.new
+  end
+
+  def execute
+    return_object
+  end
+
+  def page
+    @agent.get('https://dolarhoje.com/')
+  end
+
+  def symbol
+    @symbol ||= page.search('#cotacao .cotMoeda .symbol').last.text
+  end
+
+  def dollar_value
+    @dollar_value ||= page.search('#cotacao input#nacional').last.values.last
+  end
+
+  def return_object
+    { symbol:, value: dollar_value.tr(',', '.').to_f, description: "#{symbol} #{dollar_value}" }
+  end
+end
