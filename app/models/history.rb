c6e841b7981004b524bbadf5edf11baf6e30b314
Andre Oliveira <andreoliveirar2d2@gmail.com>
2022-10-18 02:05:47 -0300

    feat: adding newer scope

--- app/models/history.rb
+++ app/models/history.rb
@@ -3,4 +3,6 @@
 class History < ApplicationRecord
   validates_presence_of :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
+
+  scope :newer, -> { order(created_at: :desc) }
 end
