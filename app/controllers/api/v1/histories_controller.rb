066673c862c803bf4c9d5c402f99d852e620bc21
Andre Oliveira <andreoliveirar2d2@gmail.com>
2022-10-18 02:05:59 -0300

    feat: store only variation values

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -6,7 +6,7 @@ module Api
     class HistoriesController < ApplicationController
       def index
         response_obj = ConsultService.execute('dollar_today')
-        History.create(price: response_obj[:value])
+        History.create(price: response_obj[:value]) if History.newer&.last&.price != response_obj[:value]
 
         render json: response_obj
       end
